be38c51651e1aa3152c6d388877166fba800923e
Abhishek Singh <abhishek11051978@gmail.com>
2018-01-27 11:11:56 +0800

    Initial Commit

---
+++ VeganBuddy/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  VeganBuddy
+//
+//  Created by Abhishek Singh on 27/1/18.
+//  Copyright © 2018 VeganBuddy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
